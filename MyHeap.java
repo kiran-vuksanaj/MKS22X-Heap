fa6ab6fbd1c47e5f8b3f2f83fe90c9442e5a2c94
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-27 18:25:05 -0400

    children and parents helper methods

--- MyHeap.java
+++ MyHeap.java
@@ -1,6 +1,18 @@
+import java.util.NoSuchElementException;
 public class MyHeap{
   public static void main(String[] args){
     //for testing purposes
+  }
+  private static int getChildA(int index,int size){
+    if(index*2+1 < size) return index*2+1;
+    throw new NoSuchElementException("no child");
+  }
+  private static int getChildB(int index,int size){
+    if(index*2 + 2 < size) return index*2 + 2;
+    throw new NoSuchElementException("no second child");
+  }
+  private static int getParent(int index){
+    return (index - 1) / 2;
   }
   private static void pushDown(int[] data,int size,int index){
 
@@ -12,6 +24,6 @@ public class MyHeap{
 
   }
   public static void heapsort(int[] data){
-    
+
   }
 }
