ecbd5a46f816e47e5b3dffbeb4d8c70bf0684695
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-27 18:34:50 -0400

    hypothetical pushDown (may have really overcomplicated it

--- MyHeap.java
+++ MyHeap.java
@@ -3,6 +3,11 @@ public class MyHeap{
   public static void main(String[] args){
     //for testing purposes
   }
+  private static void swap(int a,int b,int[] data){
+    int temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+  }
   private static int getChildA(int index,int size){
     if(index*2+1 < size) return index*2+1;
     throw new NoSuchElementException("no child");
@@ -15,7 +20,26 @@ public class MyHeap{
     return (index - 1) / 2;
   }
   private static void pushDown(int[] data,int size,int index){
-
+    try{
+      int a = getChildA(index,size);
+      try{
+        //there are two children, so comparison is necessary
+        int b = getChildB(index,size);
+        if(data[b] > data[a]){
+          swap(index,b,data);
+          pushDown(data,size,b);
+        }else{
+          swap(index,a,data);
+          pushDown(data,size,a);
+        }
+      }catch(NoSuchElementException e){
+        //no child for second, special case of only one child
+        swap(index,a,data);
+        pushDown(data,size,a);
+      }
+    }catch(NoSuchElementException e){
+      //no children at all, pushDown is complete (do nothing)
+    }
   }
   private static void pushUp(int[] data,int index){
 
