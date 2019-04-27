c0d97967c3edf135c87c32f4346d100b07ee43d3
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-27 18:18:50 -0400

    method headers

---
+++ MyHeap.java
@@ -0,0 +1,17 @@
+public class MyHeap{
+  public static void main(String[] args){
+    //for testing purposes
+  }
+  private static void pushDown(int[] data,int size,int index){
+
+  }
+  private static void pushUp(int[] data,int index){
+
+  }
+  public static void heapify(int[] data){
+
+  }
+  public static void heapsort(int[] data){
+    
+  }
+}
